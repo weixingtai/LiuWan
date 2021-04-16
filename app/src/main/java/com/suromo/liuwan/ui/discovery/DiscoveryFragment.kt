95af3f6b944a839577a439f1f6730fb3195f1520
weixingtai <455943225@qq.com>
2021-04-16 17:08:11 +0800

    增加liuwan项目

---
+++ app/src/main/java/com/suromo/liuwan/ui/discovery/DiscoveryFragment.kt
@@ -0,0 +1,46 @@
+package com.suromo.liuwan.ui.discovery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.suromo.liuwan.databinding.FragmentDiscoveryBinding
+import com.suromo.liuwan.databinding.FragmentUserBinding
+import com.suromo.liuwan.ui.user.UserViewModel
+
+class DiscoveryFragment : Fragment() {
+
+    private lateinit var discoveryViewModel: DiscoveryViewModel
+    private var _binding: FragmentDiscoveryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        discoveryViewModel =
+            ViewModelProvider(this).get(DiscoveryViewModel::class.java)
+
+        _binding = FragmentDiscoveryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDiscovery
+        discoveryViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/suromo/liuwan/ui/discovery/DiscoveryViewModel.kt
@@ -0,0 +1,13 @@
+package com.suromo.liuwan.ui.discovery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DiscoveryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is discovery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
